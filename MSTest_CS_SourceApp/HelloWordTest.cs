6a6db137456eddd930480c1ab0be10270c589936
maheshsabnis <sabnis_m@hotmail.com>
2020-07-04 22:31:21 +0530

    Add project files.

---
+++ MSTest_CS_SourceApp/HelloWordTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest_CS_SourceApp
+{
+    [TestClass]
+    public class HelloWordTest
+    {
+
+        [TestMethod]
+        public void HelloWorldTest()
+        {
+            const string Expeted = "Hello World";
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                CS_SourceProject.Program.Main();
+                var result = sw.ToString();
+                Assert.AreEqual(Expeted,result);
+            }
+        }
+    }
+}
---
+++ MSTest_CS_SourceApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTest_CS_SourceApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTest_CS_SourceApp")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("657b6c8e-fec0-4e99-b598-780d4899c47b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
